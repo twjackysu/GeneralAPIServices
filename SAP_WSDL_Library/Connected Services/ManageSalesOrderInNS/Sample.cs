8f3a59c4c978f3967e95ceb54cc5a2ad566c5fea
Jacky Su <jacky19918@gmail.com>
2024-05-05 20:29:08 +0800

    chore: add ProjectSalesOrder sample

--- SAP_WSDL_Library/Connected Services/ManageSalesOrderInNS/Sample.cs
+++ SAP_WSDL_Library/Connected Services/ManageSalesOrderInNS/Sample.cs
@@ -99,5 +99,127 @@ namespace SAP_WSDL_Library.Connected_Services.ManageSalesOrderInNS
                 }
             ],
         };
+        public static SalesOrderMaintainRequestBundleMessage_sync ProjectSalesOrder = new SalesOrderMaintainRequestBundleMessage_sync()
+        {
+            SalesOrder =
+            [
+                new SalesOrderMaintainRequest()
+                {
+                    actionCode = ActionCode.Item01,
+                    actionCodeSpecified = true,
+                    ObjectNodeSenderTechnicalID = "S1",
+                    ReleaseAllItemsToExecution = true,
+                    ReleaseAllItemsToExecutionSpecified = true,
+                    FinishFulfilmentProcessingOfAllItems = true,
+                    FinishFulfilmentProcessingOfAllItemsSpecified = true,
+                    DataOriginTypeCode = "5",
+                    Name = new EXTENDED_Name(){
+                        Value = "ACS112162 洲美機電-台大癌醫及質子中心高低壓測試支援",
+                        languageCode = "ZF"
+                    },
+                    SalesUnitParty =  new SalesOrderMaintainRequestPartyIDParty(){
+                        actionCode = ActionCode.Item04,
+                        actionCodeSpecified = true,
+                        PartyID = new PartyID(){
+                            Value = "CS20"
+                        }
+                    },
+                    BuyerID = new BusinessTransactionDocumentID(){
+                        Value = "ACS112162"
+                    },
+                    AccountParty = new SalesOrderMaintainRequestPartyParty(){
+                        actionCode = ActionCode.Item04,
+                        actionCodeSpecified = true,
+                        PartyID = new PartyID(){
+                            Value = "D178"
+                        }
+                    },
+                    EmployeeResponsibleParty = new SalesOrderMaintainRequestPartyIDParty(){
+                        PartyID = new PartyID(){
+                            Value = "8000000169"
+                        }
+                    },
+                    PricingTerms = new SalesOrderMaintainRequestPricingTerms(){
+                        actionCode = ActionCode.Item04,
+                        actionCodeSpecified = true,
+                        CurrencyCode = "TWD",
+                        PriceDateTime = new LOCALNORMALISED_DateTime1(){
+                            timeZoneCode = "UTC+8",
+                            Value = Convert.ToDateTime("2024-03-30T10:58:00Z")
+                        },
+                        GrossAmountIndicator = false,
+                    },
+                    Item = [
+                        new SalesOrderMaintainRequestItem(){
+                            ID = "10",
+                            ProcessingTypeCode = "TPFP",
+                            ItemProduct = new SalesOrderMaintainRequestItemProduct(){
+                                actionCode = ActionCode.Item04,
+                                actionCodeSpecified = true,
+                                ProductInternalID = new ProductInternalID(){
+                                    Value = "RMPJPJ0001"
+                                }
+                            },
+                            ItemScheduleLine = [
+                                new SalesOrderMaintainRequestItemScheduleLine(){
+                                    actionCode = ActionCode.Item04,
+                                    actionCodeSpecified = true,
+                                    ID = "1",
+                                    TypeCode = "1",
+                                    Quantity = new Quantity(){
+                                        unitCode = "LS",
+                                        Value = 1
+                                    }
+                                }
+                            ],
+                            ItemServiceTerms = new SalesOrderMaintainRequestItemServiceTerms(){
+                                ProjectTaskID = new ProjectElementID(){
+                                    Value = "ACS112162"
+                                }
+                            },
+                            PriceAndTaxCalculationItem = new SalesOrderMaintainRequestPriceAndTaxCalculationItem(){
+                                actionCode = ActionCode.Item04,
+                                actionCodeSpecified = true,
+                                ItemMainPrice = new SalesOrderMaintainRequestPriceAndTaxCalculationItemItemMainPrice(){
+                                    actionCode = ActionCode.Item04,
+                                    actionCodeSpecified = true,
+                                    Rate = new Rate(){
+                                        DecimalValue = 60000,
+                                        CurrencyCode = "TWD",
+                                        BaseDecimalValue = 1,
+                                        BaseMeasureUnitCode = "LS"
+                                    }
+                                },
+                                ItemPriceComponent = [
+                                    new SalesOrderMaintainRequestPriceAndTaxCalculationItemItemPriceComponent(){
+                                        actionCode = ActionCode.Item04,
+                                        actionCodeSpecified = true,
+                                        TypeCode = new PriceSpecificationElementTypeCode(){
+                                            listID = "2",
+                                            Value = "0009"
+                                        },
+                                        Description = new SHORT_Description(){
+                                            Value = "Cost Estimate",
+                                            languageCode = "EN"
+                                        },
+                                        Rate = new Rate(){
+                                            DecimalValue = 32000,
+                                            CurrencyCode = "TWD",
+                                            BaseDecimalValue = 1,
+                                            BaseMeasureUnitCode = "LS"
+                                        },
+                                        RateBaseQuantityTypeCode = new QuantityTypeCode(){
+                                            Value = "LS"
+                                        }
+                                    }
+                                ]
+                            },
+                        }
+                    ],
+                    SendConfirmationIndicator = true,
+                    SendConfirmationIndicatorSpecified = true,
+                }
+            ]
+        };
     }
 }
