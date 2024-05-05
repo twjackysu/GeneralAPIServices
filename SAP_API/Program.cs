36dfaf23e545dd4d845c720ca35471b9b8fd4649
Jacky Su <jacky19918@gmail.com>
2024-05-05 21:55:45 +0800

    chore: add NLog, file target

--- SAP_API/Program.cs
+++ SAP_API/Program.cs
@@ -1,51 +1,76 @@
 using DotnetSdkUtilities.Factory.ResponseFactory;
 using Microsoft.OpenApi.Models;
+using NLog;
+using NLog.Web;
 using SAP_API.Common;
 using SAP_API.Configuration;
 using SAP_API.Middlewares;
-using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
-using System.Text.Json;
 
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers()
-    .AddJsonOptions(options =>
-    {
-        options.JsonSerializerOptions.PropertyNamingPolicy = null;
-    });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
+// Early init of NLog to allow startup and exception logging, before host is built
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("SAP_API start");
+
+try
 {
-    options.CustomSchemaIds(type => type.ToString());
-    options.SwaggerDoc("v1", new OpenApiInfo
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+
+    // NLog: Setup NLog for Dependency injection
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    builder.Services.AddControllers()
+        .AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+        });
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(options =>
     {
-        Version = "v1",
-        Title = "SAP API",
-        Description = "Convert SOAP api body from xml to json",
+        options.CustomSchemaIds(type => type.ToString());
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "SAP API",
+            Description = "Convert SOAP api body from xml to json",
+        });
+        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
     });
-    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-});
-builder.Services.AddScoped<IApiResponseFactory, ApiResponseFactory>();
-builder.Services.AddScoped<IMyResponseFactory, MyResponseFactory>();
+    builder.Services.AddScoped<IApiResponseFactory, ApiResponseFactory>();
+    builder.Services.AddScoped<IMyResponseFactory, MyResponseFactory>();
 
-builder.Services.AddOptions().Configure<Settings>(builder.Configuration.GetSection("Settings"));
-var app = builder.Build();
+    builder.Services.AddOptions().Configure<Settings>(builder.Configuration.GetSection("Settings"));
+    var app = builder.Build();
 
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseMiddleware<ValidateHeaderMiddleware>();
-// Configure the HTTP request pipeline.
-app.UseSwagger();
-app.UseSwaggerUI();
+    app.UseMiddleware<ExceptionMiddleware>();
+    app.UseMiddleware<ValidateHeaderMiddleware>();
+    // Configure the HTTP request pipeline.
+    app.UseSwagger();
+    app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
 
-app.UseAuthorization();
+    app.UseAuthorization();
 
-app.MapControllers();
+    app.MapControllers();
 
-app.Run();
+    app.Run();
+
+}
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
